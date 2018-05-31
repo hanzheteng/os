377f9373b8e301579dd3197cbb0adb57f783eee0
Hanzhe Teng <hanzheteng@outlook.com>
2018-05-30 23:02:04 -0700

    two shm function

--- shm.c
+++ shm.c
@@ -28,22 +28,81 @@ void shminit() {
   release(&(shm_table.lock));
 }
 
+char *
+shm_map(pde_t *pgdir, uint sz, char *mem)
+{
+  uint a;
+  a = PGROUNDUP(sz);
+  if(mappages(pgdir, (char*)a, PGSIZE, V2P(mem), PTE_W|PTE_U) < 0){
+    cprintf("allocuvm out of memory (2)\n");
+    //kfree(mem);
+    return 0;
+  }
+  return (char*)a;
+}
+
+char *
+shm_alloc(pde_t *pgdir, uint sz, char *mem)
+{
+  mem = kalloc(); //would not return 0 since max # of shm is 64 
+  if(mem == 0){
+    cprintf("allocuvm out of memory\n");
+    return 0;
+  }
+  memset(mem, 0, PGSIZE);
+  return shm_map(pgdir, sz, mem);
+}
+
+
 int shm_open(int id, char **pointer) {
+  int i;
+  char *mem = 0;
+  int exist = 0;
+  struct proc *curproc = myproc();
+  acquire(&(shm_table.lock));
 
-//you write this
-
-
-
-
-return 0; //added to remove compiler warning -- you should decide what to return
+  for(i=0; i<64; i++){
+    if(shm_table.shm_pages[i].id == id){
+      *pointer = shm_map(curproc->pgdir, curproc->sz, shm_table.shm_pages[i].frame); 
+      shm_table.shm_pages[i].refcnt += 1;
+      curproc->sz = PGROUNDUP(curproc->sz) + PGSIZE; 
+      exist = 1;
+      break;
+    }
+  }
+ if(!exist){
+    for(i=0; i<64; i++){
+      if(shm_table.shm_pages[i].id != 0)
+        continue;
+      shm_table.shm_pages[i].id = id; // should assign the given id
+      *pointer = shm_alloc(curproc->pgdir, curproc->sz, mem);
+      shm_table.shm_pages[i].frame = mem;
+      shm_table.shm_pages[i].refcnt = 1;
+      curproc->sz = PGROUNDUP(curproc->sz) + PGSIZE;
+      break;
+    }
+    if(shm_table.shm_pages[63].id != 0){
+      cprintf("shared memory is full\n");
+      return -1;
+    }
+  }
+  release(&(shm_table.lock));
+  return 0; //added to remove compiler warning -- you should decide what to return
 }
 
 
 int shm_close(int id) {
-//you write this too!
-
-
-
-
-return 0; //added to remove compiler warning -- you should decide what to return
+  int i;
+  acquire(&(shm_table.lock));
+  for(i=0; i<64; i++){
+    if(shm_table.shm_pages[i].id == id){
+      if((shm_table.shm_pages[i].refcnt -= 1) == 0){
+        shm_table.shm_pages[i].id = 0;
+        shm_table.shm_pages[i].frame = 0;
+      }
+    }
+  }
+  release(&(shm_table.lock));
+  return 0; //added to remove compiler warning -- you should decide what to return
 }
+
