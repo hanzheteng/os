9960585dd784037da1603fb326d970c79fde017d
enlighten5 <1483432078@qq.com>
2018-05-30 21:15:59 -0700

    lab4

--- shm.c
+++ shm.c
@@ -27,23 +27,75 @@ void shminit() {
   }
   release(&(shm_table.lock));
 }
-
+char*
+alloc_shm(pde_t *pgdir, uint oldsz, uint newsz)
+{
+  char *mem;
+  if(newsz >= KERNBASE)
+    return 0;
+  if(newsz < oldsz)
+    return oldsz;
+  uint a;
+  a = PGROUNDUP(OLDSZ); 
+  mem = kalloc();
+  if(mem == 0){
+    cprintf("allocuvm out of memory(1)\n");
+    return 0;
+  }
+  memset(mem, 0, PGSIZE);
+  if(mappages(pgdir, (char*)a, PGSIZE, V2P(mem), PTE_W|PTE_U) < 0){
+    cprintf("allocuvm out of memory (2)\n");  
+    return 0;
+  }
+  return mem;
+}
 int shm_open(int id, char **pointer) {
-
+  struct proc *curproc = myproc();
+  uint sz;
+  int i;
+  char *mem;
 //you write this
+  sz = PGROUNDUP(curproc->sz);
+  acquire(&(shm_table.lock));
+  for (i = 0; i< 64; i++) {
+    if(shm_table.shm_pages[i].id == id){
+      if(mappages(curproc->pgdir, (char*)sz, PGSIZE, v2p(shm_table.shm_pages[i].frame), PTE_W|PTE_U) < 0)      {
+        cprintf("mappages failed \n");
+        return 0;
+      }
+      shm_table.shm_pages[i].refcnt++;
+      *pointer = (char *)curproc->sz; 
+      curproc->sz += PGSIZE;
+      return 0;
+    }
+  }
+  for(i = 0, i < 64; i++){
+    if(shm_table.shm_pages[i].id == 0){
+      shm_table.shm_pages[i].id = id;
+      shm_table.shm_pages[i].refcnt ++;
+      shm_table.shm_pages[i].frame = alloc_shm(curproc->pgdir, sz, sz + PGSIZE); 
+      curproc->sz += PGSIZE;
+      shm_table.shm_pages[i].refcnt++;
+      *pointer = (char *)sz;
+      break;
+    }
 
-
-
-
-return 0; //added to remove compiler warning -- you should decide what to return
+  }
+  release(&(shm_table.lock));
+  if(!*pointer)
+  cprintf("shm_open fail");
+  return 0; //added to remove compiler warning -- you should decide what to return
 }
 
 
 int shm_close(int id) {
 //you write this too!
+  if(shm_table.shm_pages[i].id == id){
+    if(shm_table.shm_pages[i].refcnt > 0)
+      shm_table.shm_pages[i].refcnt--;
+    else
+      shm_table.shm_pages[i].refcnt = 0;
+  }
 
-
-
-
-return 0; //added to remove compiler warning -- you should decide what to return
+  return 0; //added to remove compiler warning -- you should decide what to return
 }
